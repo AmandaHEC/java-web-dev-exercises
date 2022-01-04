966ad80e354e6cf3dc673ae13c77485bc54ec740
AmandaHEC <93448955+AmandaHEC@users.noreply.github.com>
2022-01-03 20:27:34 -0600

    src

---
+++ src/exercises/areaOfRectangle.java
@@ -0,0 +1,27 @@
+package exercises;
+import java.util.Scanner;
+
+public class areaOfRectangle {
+    public static void main(String[] args) {
+        //int length;
+        //int width;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the length of the rectangle.");
+        Double length = input.nextDouble();
+
+        System.out.println("Enter the width of the rectangle.");
+        Double width = input.nextDouble();
+
+        Double area = length * width;
+
+        System.out.println("The area of the rectangle is : "+ area +"units squared. ");
+
+
+
+
+
+
+
+    }
+}
